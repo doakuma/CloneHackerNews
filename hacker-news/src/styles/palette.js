b7358addb6e0ee602cd138572a4d1545019c03be
doakuma <doakuma@gmail.com>
2020-02-01 22:03:40 +0900

    haker news start

---
+++ hacker-news/src/styles/palette.js
@@ -0,0 +1,15 @@
+// ---
+export const colorDark = {
+    background: '#272727',
+    backgroundSecondary: '#393c3e',
+    text: '#bfbebe',
+    textSecondary: '#838886',
+    border: '#272727'
+}
+export const colorLight = {
+    background: '#eaeaea',
+    backgroundSecondary: '#f8f8f8',
+    text: '#846886',
+    textSecondary: '#aaaaaa',
+    border: '#eaeaea'
+}
